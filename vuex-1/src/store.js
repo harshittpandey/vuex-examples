f83512713b4fc3b47b83b8903cacb1164fbe3015
harshittpandey <harshitpandey678@gmail.com>
2019-02-07 01:20:41 +0530

    completed todo using vuex

---
+++ vuex-1/src/store.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    title: "My custom Title",
+    links: [
+      "http://google.com",
+      "http://youtube.com",
+      "http://harshittpandey.github.io"
+    ]
+  },
+  getters: {
+    countLinks: state=> {
+      return state.links.length
+    }
+  },
+  mutations: {
+    ADD_LINK: (state, link)=> {
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link) => {
+            state.links.splice(link, 1)
+    },
+    REMOVE_ALL: (state)=> {
+        state.links= []
+    }
+  },
+  actions: {
+      removeLink: (context, link) => {
+          context.commit ("REMOVE_LINK", link)
+      },
+      removeAll ({commit}) {
+          return new Promise((resolve, reject)=> {
+              setTimeout(()=> {
+                  commit("REMOVE_ALL")
+                  resolve()
+              },1500)
+          })
+      }
+
+  }
+})
