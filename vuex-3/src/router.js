d5c1226b06c0abde78eae0212cc2c3a6b288a2a1
harshittpandey <harshitpandey678@gmail.com>
2019-02-18 15:45:29 +0530

    created shopping cart project in vuex-3

---
+++ vuex-3/src/router.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './components/Home.vue'
+import Products from './components/products.vue'
+import Cart from './components/cart.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: Products
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Cart
+    }
+  ]
+})
